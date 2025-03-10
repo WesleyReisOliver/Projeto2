685ebe46d313cc4221eadc8d331b9f0b93d25f72
WesleyReisOliver <159446382+WesleyReisOliver@users.noreply.github.com>
2025-03-10 20:04:27 -0300

    Add files via upload

---
+++ projetoWeb-1/src/main/java/br/ueg/trindade/desenvolvimento/projetoWeb_1/ProjetoWeb1Application.java
@@ -0,0 +1,13 @@
+package br.ueg.trindade.desenvolvimento.projetoWeb_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWeb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWeb1Application.class, args);
+	}
+
+}
---
+++ projetoWeb-1/src/test/java/br/ueg/trindade/desenvolvimento/projetoWeb_1/ProjetoWeb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.trindade.desenvolvimento.projetoWeb_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWeb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
